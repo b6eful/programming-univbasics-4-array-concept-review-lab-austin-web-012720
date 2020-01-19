9d93a2035fcdca1ce9be9041ae9bde72ffbedcc6
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 00:35:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ def find_element_index(array, value_to_find)
 end
 def find_max_value(array)
   x = 0
-  array.length.times { |index| x = array[index] if array[index > x]}
+  array.length.times { |index| x = array[index] if array[index > x] }
   x
 end
 
