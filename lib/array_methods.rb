fd100bd2bbb05bc07ed10250447fd787d95fcc89
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 00:25:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,9 @@ def find_element_index(array, value_to_find)
     count += 1
 end
 def find_max_value(array)
+  x = 0
   array.length.times { |index| x = array[index] if array |index| > x}
+  x
 end
 
 def find_min_value(array)
